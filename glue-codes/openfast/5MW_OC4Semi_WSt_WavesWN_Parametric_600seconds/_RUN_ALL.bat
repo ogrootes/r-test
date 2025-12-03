ea6a09095344f6dc21cee31b0a8c69934e555c3e
Ole Grootes <ogrootes@tudelft.nl>
2025-12-03 16:18:01 +0100

    600s parametrization run 5MW OC4 semisub turbine

---
+++ glue-codes/openfast/5MW_OC4Semi_WSt_WavesWN_Parametric_600seconds/_RUN_ALL.bat
@@ -0,0 +1,7 @@
+..\..\..\..\openfast_x64.exe 000_ws04.0_om2.50.fst
+..\..\..\..\openfast_x64.exe 001_ws05.0_om7.50.fst
+..\..\..\..\openfast_x64.exe 002_ws08.0_om9.00.fst
+..\..\..\..\openfast_x64.exe 003_ws10.0_om11.30.fst
+..\..\..\..\openfast_x64.exe 004_ws12.0_om12.10.fst
+..\..\..\..\openfast_x64.exe 005_ws14.0_om12.10.fst
+..\..\..\..\openfast_x64.exe 006_ws16.0_om12.10.fst
